bca85006d1760e42af8f32c5493960ce30791c7f
Benjamin Woodward <benjamin.woodward@cvisionconsulting.com>
2022-05-03 11:38:08 -0400

    Fix path for returned url and add logging for existing file

--- app/main.py
+++ app/main.py
@@ -61,17 +61,31 @@ def crop(image: ImageCrop):
 
     url = images.find_by_uuid(image.uuid).url
 
+<<<<<<< HEAD
     path_string = f'/static/{image.uuid}_{image.x1}_{image.y1}_{image.x2}_{image.y2}.png'
     if exists(path_string):
         pass  
+=======
+    if exists(f'/static-files/{image.get("uuid")}_{image.get("x1")}_{image.get("y1")}_{image.get("x2")}_{image.get("y2")}.png'):
+        logger.info("Requested file exists, skipping crop operation")
+>>>>>>> Fix path for returned url and add logging for existing file
     else:
         img = Image.open(requests.get(url, stream=True).raw)
         img_crop = img.crop((image.x1,image.y1,image.x2,image.y2))
         img_crop.save(path_string) 
 
+<<<<<<< HEAD
     data = {'url' : path_string} 
     
     return JSONResponse(content=jsonable_encoder(data))
+=======
+    data = {'url' : f'/static/{image.get("uuid")}_{image.get("x1")}_{image.get("y1")}_{image.get("x2")}_{image.get("y2")}.png'}
+    headers = {"Access-Control-Allow-Origin": "*", 
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+            "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"}
+    return JSONResponse(content=jsonable_encoder(data), headers=headers)
+>>>>>>> Fix path for returned url and add logging for existing file
 
 # for debugging purposes, it's helpful to start the Flask testing
 # server (don't use this for production)
